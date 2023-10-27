5056521aca69d9d17a6b6291d32567a153fa0c7a
Austinet <austinetsplendour@gmail.com>
2023-10-27 02:42:34 +0100

    ch: optimized codebase, removed unused styles

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,7 @@
 const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.querySelector(".nav-links");
 
+// Toggle Navbar - Mobile Screen
 menuBtn.addEventListener("click", function () {
   const navClassList = navLinks.classList;
 
